471dde3568a215ae36671534ddf1fcfac25a73a3
LuizFAraujo <luizfaraujo.eng@gmail.com>
2022-08-12 00:51:16 -0300

    Configuração Inicial - Ambiente Web React

---
+++ FRONT/sys-web-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <h2>ARJ CONTROL SYSTEM</h2>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
